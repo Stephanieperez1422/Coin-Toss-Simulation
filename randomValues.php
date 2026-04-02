0792d67ac52b776dbde6ff87fd1ae15211a137df
Stephanieperez1422 <Stephanie.perez05@stu.bcc.cuny.edu>
2026-04-02 17:07:09 -0400

    Add files via upload

---
+++ randomValues.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Random Values</title>
+</head>
+<body>
+
+<h2>Random Number Generator</h2>
+
+<?php
+echo "Random number (1-27): " . rand(1, 27) . "<br><br>";
+echo "Random number (1-178): " . rand(1, 178) . "<br><br>";
+echo "Random number (1-600): " . rand(1, 600);
+?>
+
+</body>
+</html>
